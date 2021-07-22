10453898f44c8d64d8877df386cd95b086ac4c5a
Adrian Sims <adriansims62781@gmail.com>
2021-07-22 20:55:20 +0200

    Add project files.

---
+++ ShExec/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace ShExec
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = args[0],
+                    UseShellExecute = true,
+                    Arguments = string.Join("U+0020", args.Skip(1))
+                };
+
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
