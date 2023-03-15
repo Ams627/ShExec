f06308834c3aaa59dd251df6df8b4ccde3e1bb00
Adrian Sims <adriansims62781@gmail.com>
2023-03-15 21:10:50 +0100

    Make App Paths work

--- ShExec/Program.cs
+++ ShExec/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Diagnostics;
+using WPath;
 
 namespace ShExec
 {
-    class Program
+    internal record App(string AppName, string Path);
+
+    internal class Program
     {
         private static void Main(string[] args)
         {
@@ -15,9 +19,29 @@ namespace ShExec
                 {
                     throw new Exception("You must supply a filename to pass to ShellExecute");
                 }
+
+                var appKeys = @"HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+
+                var appDict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+                foreach (var key in RegLib.GetSubKeyNames(appKeys))
+                {
+                    // get app name:
+                    var k = Path.GetFileNameWithoutExtension(key);
+                    var ke = Path.GetFileName(key);
+
+                    // get app path:
+                    var value = RegLib.GetDefaultValue(key);
+                }
+
+                if (!appDict.TryGetValue(args[0], out string appPath))
+                {
+                    appPath = args[0];
+                }
+
                 var psi = new ProcessStartInfo
                 {
-                    FileName = args[0],
+                    FileName = appPath,
                     UseShellExecute = true,
                     Arguments = string.Join("U+0020", args.Skip(1))
                 };
